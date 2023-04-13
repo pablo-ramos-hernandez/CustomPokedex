7f57c9f542de3fd8cfc2abb9fc2f813d0f5e90a9
pablo-ramos-hernandez <usr2741@salesianos-lacuesta.net>
2023-04-13 12:19:09 +0100

    Create class Pokemon
    Creacion de la clase Pokemon y sus metodos

---
+++ CustomPokedex/src/Pokemon.java
@@ -0,0 +1,60 @@
+public class Pokemon {
+    private String nombre;
+    private String tipo;
+    private int altura;
+    private int peso;
+    private String descripcion;
+
+    // Creacion de un constructor
+
+    public Pokemon(String nombre, String tipo, int altura, int peso, String descripcion) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.altura = altura;
+        this.peso = peso;
+        this.descripcion = descripcion;
+    }
+
+    // Creacion de constructor con solo dos valores
+
+    public Pokemon(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    // Creacion de los getters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    //Creacion de los setters
+
+    public void setAltura(int nuevaAltura) {
+        this.altura = nuevaAltura;
+    }
+
+    public void setPeso(int nuevoPeso) {
+        this.peso = nuevoPeso;
+    }
+
+    public void setDescripcion(String nuevaDescripcion) {
+        this.descripcion = nuevaDescripcion;
+    }
+}
